256e398acf2b4e5f299dde370677087ad464ab24
E-know <eknow25@kakao.com>
2024-05-31 13:08:40 +0900

    FAQ 항목 추가

--- K-Spam/View/Main/HomeView.swift
+++ K-Spam/View/Main/HomeView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct HomeView: View {
     @State private var showSettingInfo = false
     @State private var showFilterLogicView = false
+    @State private var showWantNotFilter = false
     
     var body: some View {
         VStack {
@@ -29,35 +30,37 @@ struct HomeView: View {
                     .foregroundStyle(Color.green)
                     .padding(.bottom, 8)
                 
-                HStack {
-                    Text("∙ K-Spam이 작동 안해요.")
-                        .font(.system(size: 14, weight: .light))
-                    
-                    Button(action: { showSettingInfo.toggle() }) {
-                        Image(systemName: "info.circle")
-                    }
-                    Spacer()
-                }
-                .padding(.horizontal)
+                FAQElement(text: "K-Spam이 작동 안해요", showModal: $showSettingInfo)
                 
-                HStack {
-                    Text("∙ K-Spam 작동 순서를 알고 싶어요.")
-                        .font(.system(size: 14, weight: .light))
-                    Button(action: { showFilterLogicView.toggle() }) {
-                        Image(systemName: "info.circle")
-                    }
-                    Spacer()
-                }.padding(.horizontal)
+                FAQElement(text: "K-Spam 작동 순서를 알고 싶어요.", showModal: $showFilterLogicView)
                 
-
+                FAQElement(text: "필터하고 싶지 않은 번호가 있어요.", showModal: $showWantNotFilter)
             }
         }
         .sheet(isPresented: $showSettingInfo) { OnboardingPage4() }
         .sheet(isPresented: $showFilterLogicView) {
             FilterLogicView()
                 .presentationDetents([.medium])
-                .presentationCornerRadius(20)
         }
+        .sheet(isPresented: $showWantNotFilter, content: {
+            OnboardingPage2()
+                .presentationDetents([.height(UIScreen.main.bounds.size.height * 3 / 4)])
+        })
+    }
+    
+    @ViewBuilder
+    private func FAQElement(text: String, showModal: Binding<Bool>) -> some View {
+        HStack {
+            Text("∙ \(text)")
+                .font(.system(size: 14, weight: .light))
+            
+            Button(action: { showModal.wrappedValue.toggle() }) {
+                Image(systemName: "info.circle")
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
     }
 }
 
