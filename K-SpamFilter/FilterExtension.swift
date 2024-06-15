597f847aa9e9ea4e0d9d6aa6924adc2475fd8d80
E-know <eknow25@kakao.com>
2024-06-16 01:02:14 +0900

    [필터] 과도하게 필터가 적용되었던 문제 수정 (카지노 / 국제발신)

--- K-SpamFilter/FilterExtension.swift
+++ K-SpamFilter/FilterExtension.swift
@@ -12,191 +12,42 @@ extension MessageFilterExtension {
         guard let messageBody = queryRequest.messageBody else { return (.none, .none) }
         let messageLines = messageBody.split(separator: "\n").map { String($0) }
         
-        if checkWhiteFilterWords(messageLines: messageLines) {
+        if filter.checkWhiteFilterWords(messageLines: messageLines) {
             return (.allow, .none)
         }
         
-        if checkKSpam(messageLines: messageLines) {
+        if filter.checkKSpam(messageLines: messageLines) {
             return (.junk, .none)
         }
         
         if UserDefaultsManager.shared.getBool(key: .InternationalSend) {
-            if let sender = queryRequest.sender, !isFromKorea(sender: sender) {
-                return (.junk, .none)
+            if let sender = queryRequest.sender {
+                let res = filter.isFromKorea(sender: sender)
+                guard res != .no else { return (.junk, .none) }
             }
             
+            // SKT 전용 국제발신
             if let res = messageLines.first?.elementsEqual("[국제발신]"), res {
                 return (.junk, .none)
             }
         }
         
         if UserDefaultsManager.shared.getBool(key: .ChargeCasino) {
-            if checkChargeCasino(messageLines) {
+            if filter.checkChargeCasino(messageLines) {
                 return (.junk, .none)
             }
         }
         
-        if checkBlackFilterWords(messageLines: messageLines) {
+        if filter.checkBlackFilterWords(messageLines: messageLines) {
             return (.junk, .none)
         }
         
         if UserDefaultsManager.shared.getBool(key: .Advertise) {
-            if checkAdvertise(messageLines: messageLines) {
+            if filter.checkAdvertise(messageLines: messageLines) {
                 return (.junk, .none)
             }
         }
         
-        
         return (.none, .none)
-    }
-    
-    private func checkWhiteFilterWords(messageLines: [String]) -> Bool {
-        let whiteFilterWords = UserDefaultsManager.shared.getStrings(key: .WhiteFilterWords)
-        guard !whiteFilterWords.isEmpty else { return false }
-        
-        for line in messageLines {
-            for whiteFilterWord in whiteFilterWords {
-                if line.contains(whiteFilterWord) {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    
-    private func checkKSpam(messageLines: [String]) -> Bool {
-        var strDic = [String: Int]()
-        
-        for line in messageLines {
-            if checkLink(line) || checkVowelConsonants(line) {
-                return true
-            }
-            
-            if Set(line).count == 1 && line.count > 3 {
-                strDic[line, default: 0] += 1
-                
-                if let count = strDic[line], count >= 2 {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    
-    // TODO: ㅋㅋ ㅎㅎ ㅠㅠ 같은 일상어 제거하기
-    private func checkVowelConsonants(_ str: String) -> Bool {
-        let consonants = Set<UInt32>("ㄴㄷㄹㅁㅂㅅㅇㅈㅊㅌㅍ".unicodeScalars.map { $0.value }) // ㅋㅋㅋㅋ ㅎㅎㅎㅎ ㄱㄱ 는 생략
-        let vowels = Set<UInt32>("ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ".unicodeScalars.map { $0.value })
-        
-        for unicode in str.unicodeScalars {
-            if consonants.contains(unicode.value) || vowels.contains(unicode.value) {
-                return true
-            }
-        }
-        
-        return false
-    }
-    
-    private func checkLink(_ str: String) -> Bool {
-        var que = LinkQueue()
-        var containSpecialChar = false
-        
-        for char in str.unicodeScalars {
-            let unicode = Int(char.value)
-            containSpecialChar = containSpecialChar || 9398...9423 ~= unicode || 9424...9449 ~= unicode
-            que.append(Character(char))
-            if que.checkCom() && containSpecialChar {
-                return true
-            }
-        }
-        return false
-    }
-    
-    private func checkChargeCasino(_ lines: [String]) -> Bool {
-        // 모든 항목을 Int로 변환이 가능하면 True 아니면 false
-        func checkInt(elements: [String]) -> Bool {
-            for element in elements {
-                guard let _ = Int(element) else { return false }
-            }
-            return true
-        }
-        
-        
-        for line in lines {
-            let charsSlash = line.split(separator: "/")
-            
-            if charsSlash.count > 1 {
-                let elements = charsSlash.map { $0.split(separator: "+").map { String($0) } }.flatMap{ $0 }
-                var count = 0
-                
-                for line in elements {
-                    if checkInt(elements: line.split(separator: "+").map { String($0) }) {
-                        count += 1
-                    }
-                    if count == 2 {
-                        return true
-                    }
-                }
-            }
-            
-            let charsSpace = line.split(separator: " ")
-            if charsSpace.count > 1 {
-                let elements = charsSlash.map { $0.split(separator: "+").map { String($0) } }.flatMap{ $0 }
-                
-                if checkInt(elements: elements) {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    
-    private func checkBlackFilterWords(messageLines: [String]) -> Bool {
-        let blackFilterWords = UserDefaultsManager.shared.getStrings(key: .BlackFilterWords)
-        guard !blackFilterWords.isEmpty else { return false }
-        
-        for line in messageLines {
-            for blackFilterWord in blackFilterWords {
-                if line.contains(blackFilterWord) {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    
-    private func checkAdvertise(messageLines: [String]) -> Bool {
-        guard let firstLine = messageLines.first else { return false }
-        
-        if firstLine.contains("광고") { return true }
-        
-        guard messageLines.count >= 2 else { return false }
-        
-        return messageLines[1].contains("광고")
-    }
-    
-    private func isFromKorea(sender: String) -> Bool {
-        let nationalCode = sender[sender.startIndex..<sender.index(sender.startIndex, offsetBy: 3)]
-        
-        return nationalCode == "+82"
-    }
-}
-
-struct LinkQueue {
-    var arr = [Character]()
-    var idx = 0
-    mutating func append(_ ele: Character) {
-        arr.append(ele)
-    }
-    
-    func checkCom() -> Bool {
-        guard arr.count >= 4 else { return false }
-        let lastIdx = arr.count - 1
-        var res = arr[lastIdx] == "."
-        res = res && (arr[lastIdx - 2] == "c" || arr[lastIdx - 2] == "C" || arr[lastIdx - 2] == "©" || arr[lastIdx - 2] == "ⓒ" || arr[lastIdx - 2] == "Ⓒ")
-        res = res && (arr[lastIdx - 1] == "o" || arr[lastIdx - 1] == "O" || arr[lastIdx - 1] == "0" || arr[lastIdx - 1] == "ⓞ" || arr[lastIdx - 1] == "Ⓞ")
-        res = res && (arr[lastIdx] == "m" || arr[lastIdx] == "M" || arr[lastIdx] == "ⓜ" || arr[lastIdx] == "Ⓜ")
-        
-        return res
     }
 }
